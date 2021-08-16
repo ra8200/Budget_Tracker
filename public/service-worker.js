714e4fe4d43799d0d86bf803d85586e3e99494ea
ra8200 <ra8200@hotmail.com>
2021-08-15 20:32:04 -0400

    added fetch process to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -36,3 +36,38 @@ self.addEventListener("activate", function (evt) {
   );
   self.clients.claim();
 });
+
+// fetching cached data
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      catches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.matchI(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          return catches.match("/");
+        }
+      });
+    })
+  );
+});
