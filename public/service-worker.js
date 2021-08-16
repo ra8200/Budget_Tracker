149d88c81cb67c9a8d99f14b2e691a973ef9acce
ra8200 <ra8200@hotmail.com>
2021-08-15 20:04:43 -0400

    added install and activate  fucntions to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,3 +6,33 @@ const FILES_TO_CACHE = [
   "/public/icons/icon-192x192.png",
   "/public/icons/icon-512x512.png",
 ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//installs cached function
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    cashes.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached.");
+    })
+  );
+  self.skipWaiting();
+});
+
+//activates caching
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing previous data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
